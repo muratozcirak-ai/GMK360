4a93e9e3c9405564770e6636e8a310d8255c5bc3
Murat Ozcirak <murat@example.com>
2026-09-14 19:03:27 +0300

    Initialize Select2 for multiple selection box

---
+++ FixSelect2.py
@@ -0,0 +1,22 @@
+﻿import codecs
+
+filepath = r'C:\Users\murat\source\repos\GMK360\GMK360.Web\Views\Agenda\Create.cshtml'
+with codecs.open(filepath, 'r', 'utf-8-sig') as f:
+    content = f.read()
+
+script_addition = '''
+        .ready(function() {
+            if ($.fn.select2) {
+                .select2.select2({
+                    theme: 'bootstrap-5',
+                    width: '100%'
+                });
+            }
+        });
+
+        let itemIndex = 1;'''
+
+content = content.replace('let itemIndex = 1;', script_addition)
+
+with codecs.open(filepath, 'w', 'utf-8-sig') as f:
+    f.write(content)
---
+++ FixSelect2Final.py
@@ -0,0 +1,24 @@
+﻿import codecs
+
+filepath = r'C:\Users\murat\source\repos\GMK360\GMK360.Web\Views\Agenda\Create.cshtml'
+with codecs.open(filepath, 'r', 'utf-8-sig') as f:
+    content = f.read()
+
+new_script = '''
+        $(document).ready(function() {
+            if ($.fn.select2) {
+                $('.select2').select2({
+                    theme: 'bootstrap-5',
+                    width: '100%',
+                    placeholder: 'Seçim yapınız...'
+                });
+            }
+        });
+
+        let itemIndex = 0;'''
+
+if 'theme: \'bootstrap-5\'' not in content:
+    content = content.replace('let itemIndex = 0;', new_script)
+
+with codecs.open(filepath, 'w', 'utf-8-sig') as f:
+    f.write(content)
